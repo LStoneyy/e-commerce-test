c16f1e7a1edec691ea8a29ac28dc51aed7454b22
LStoneyy <123068985+LStoneyy@users.noreply.github.com>
2025-11-28 11:45:07 +0100

    Added cart views + basic html structure

--- shop/urls.py
+++ shop/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("products/", views.products, name="products"),
     path("products/product/<int:pk>/", views.product_detail, name="product_detail"),
-    path("cart/", views.home, name="cart"),
-    path("cart/add/<int:product_id>/", views.home, name="add_to_cart"),
-    path("cart/remove/<int:product_id>/", views.home, name="remove_from_cart"),
-    path("cart/update/<int:product_id>/", views.home, name="update_cart"),
+    path("cart/", views.cart, name="cart"),
+    path("cart/add/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
+    path("cart/remove/<int:product_id>/", views.remove_from_cart, name="remove_from_cart"),
+    path("cart/update/<int:product_id>/", views.update_cart, name="update_cart"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
+from .cart import Cart
 
 def home(request):
     return render(request, "shop/home.html")
@@ -11,3 +12,31 @@ def products(request):
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, "shop/product_detail.html", {"product": product})
+
+def cart(request):
+    cart = Cart(request)
+    return render(request, "shop/cart.html", {"cart": cart})
+
+# call cart class functions
+def add_to_cart(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+
+    cart.add(product=product, quantity=1)
+    return redirect("products")
+
+def remove_from_cart(request, product_id):
+    cart = Cart(request)
+    cart.remove(product_id)
+    return redirect("cart")
+
+def update_cart(request, product_id):
+    cart = Cart(request)
+    quantity = int(request.POST.get("quantity", 1))
+
+    if quantity > 0:
+        cart.update(product_id, quantity)
+    else:
+        cart.remove(product_id)
+    
+    return redirect("cart")
