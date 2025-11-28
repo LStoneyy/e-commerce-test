186b1d97e0a8bd8ba1c30fb0e61c557f303f552c
LStoneyy <123068985+LStoneyy@users.noreply.github.com>
2025-11-28 09:35:12 +0100

    Added media

--- config/settings/base.py
+++ config/settings/base.py
@@ -86,3 +86,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
