c2ff19cf0173fcd8c0d1eb590e4e55e158c855c8
LStoneyy <123068985+LStoneyy@users.noreply.github.com>
2025-11-28 10:47:17 +0100

    Registered models in admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Product, Order, OrderItem
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "stock", "created_at"]
+    search_fields = ["name", "description"]
+    list_filter = ["created_at"]
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "total_price", "status", "created_at"]
+    search_fields = ["status", "created_at"]
+    list_filter = ["user__username"]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ["order", "product", "quantity", "price"]
